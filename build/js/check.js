03142b5ff9194bd118ca0c3f47e73d759af7cba8
loompa <anya_kasheeva@rambler.ru>
2016-05-29 00:23:38 +0300

    Модуль 1 Задание 2
    corrected mistakes

--- build/js/check.js
+++ build/js/check.js
@@ -1,35 +1,33 @@
 var getMessage = function (a,b) {
-	if ( (typeof a) === "boolean") {
+	if ((typeof a) === 'boolean') {
 		if (a) {
-			return "Я попал в " + b ;
+			return 'Я попал в ' + b;
 		}
 		else {
-			return "Я никуда не попал" ;
+			return 'Я никуда не попал';
 		}
 	}
 
-	if ( (typeof a) === "number" ) {
-		return "Я прыгнул на " + a*100 + " сантиметров" ;
+	if ((typeof a) === "number") {
+		return 'Я прыгнул на ' + a*100 + ' сантиметров';
 	}
 
 	if (Array.isArray(a) && Array.isArray(b)) {
 		
 		var length = 0 ;
-			for (var i=0; i < a.length; i++) {
-				length = length + a[i] * b[i] ;
+			for (var i = 0; i < a.length; i++) {
+				length = length + a[i] * b[i];
 			}
-		return "Я прошёл " + length + " метров" ;
-	}
-	else {
+		return 'Я прошёл ' + length + ' метров';
+	} else {
 		if (Array.isArray(a)) {
 			var sum = 0 ;
-												//массив [a] состоит из 4 элементов
-			for (var i=0; i < a.length; i++) {	//1 2 2 4
-				sum = sum + a[i] ;   			//сумма в цикле на первом шаге и втором
-				} 					  			//верная (1 и 3), на третьем почему-то
-		return "Я прошел "  + sum + " шагов" ;  //3+2=6 и в итоге sum=10
+												
+			for (var i=0; i < a.length; i++) {	
+				sum = sum + a[i] ;   			
+				} 					  			
+		return 'Я прошел '  + sum  + ' шагов';  
 		}
    }
 
 }
-
