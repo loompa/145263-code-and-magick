94871a689a3ebd2745bba492ab09f9bccb503a4d
loompa <anya_kasheeva@rambler.ru>
2016-05-29 17:14:04 +0300

    Модуль 1 Задание 2
    changed

--- build/js/check.js
+++ build/js/check.js
@@ -1,33 +1,29 @@
-var getMessage = function (a,b) {
-	if ((typeof a) === 'boolean') {
-		if (a) {
-			return 'Я попал в ' + b;
-		}
-		else {
-			return 'Я никуда не попал';
-		}
-	}
+var getMessage = function(a,b) {
+  if ((typeof a) === 'boolean') {
+    if (a) {
+      return 'Я попал в ' + b;
+    } else {
+      return 'Я никуда не попал';
+      }
+    }
 
-	if ((typeof a) === "number") {
-		return 'Я прыгнул на ' + a*100 + ' сантиметров';
-	}
+  if ((typeof a) === 'number') {
+    return 'Я прыгнул на ' + a*100 + ' сантиметров';
+  }
 
-	if (Array.isArray(a) && Array.isArray(b)) {
-		
-		var length = 0 ;
-			for (var i = 0; i < a.length; i++) {
-				length = length + a[i] * b[i];
-			}
-		return 'Я прошёл ' + length + ' метров';
-	} else {
-		if (Array.isArray(a)) {
-			var sum = 0 ;
-												
-			for (var i=0; i < a.length; i++) {	
-				sum = sum + a[i] ;   			
-				} 					  			
-		return 'Я прошел '  + sum  + ' шагов';  
-		}
-   }
-
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var length = 0;
+    for (var i = 0; i < a.length; i++) {
+      length = length + a[i] * b[i];
+    }
+    return 'Я прошёл ' + length + ' метров';
+  } else {
+      if (Array.isArray(a)) {
+        var sum = 0;
+        for (var i = 0; i < a.length; i++) {	
+          sum = sum + a[i];
+        }
+      return 'Я прошел ' + sum + ' шагов';
+      }
+    }
 }
