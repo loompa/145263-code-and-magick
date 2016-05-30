26ad0dde1362b9c676b6fe9eba370a76eaa94622
loompa <anya_kasheeva@rambler.ru>
2016-05-30 13:47:19 +0300

    Модуль 1 Задание 2
    with reduce method

--- build/js/check.js
+++ build/js/check.js
@@ -4,26 +4,24 @@
         return 'Я попал в ' + b;
       } else {
         return 'Я никуда не попал';
-        }
       }
+    }
 
     if ((typeof a) === 'number') {
       return 'Я прыгнул на ' + a*100 + ' сантиметров';
     }
 
-    if (Array.isArray(a) && Array.isArray(b) && (a.length === b.length)) {
-      var length = 0;
-      for (var i = 0; i < a.length; i++) {
-        length = length + a[i] * b[i];
-      }
-      return 'Я прошёл ' + length + ' метров';
+    if (Array.isArray(a) && Array.isArray(b)) {
+      var length = a.reduce(function(previousValue, currentValue, currentIndex) {
+        return previousValue + (currentValue*b[currentIndex]); 
+      }, 0);
+      return 'Я прошёл ' + length + ' шагов';
     } else {
-  	      if (Array.isArray(a)) {
-          var sum = 0;
-          for (var i = 0; i < a.length; i++) {	
-            sum = sum + a[i];
-          }
+      if (Array.isArray(a)) {
+        var sum = a.reduce(function(previousValue, currentValue) {
+          return previousValue + currentValue; 
+        });
         return 'Я прошел ' + sum + ' шагов';
-        }
       }
+    }
   }
