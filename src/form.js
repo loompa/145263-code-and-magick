8e416109834fbe09b98551e2ab15f31aeb8a4d7f
loompa <anya_kasheeva@rambler.ru>
2016-06-08 15:33:13 +0300

    Начала задание - сделала обязательным поле name; обязательным поле review в зависимости от оценки

--- src/form.js
+++ src/form.js
@@ -4,6 +4,19 @@
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
+  var form = document.querySelector('.review-form');
+  var field = form.querySelector('.review-form-group-mark');
+  var radio = form.elements['review-mark'];
+  var name = form.elements['review-name'];
+  var text = form.elements['review-text'];
+
+  document.getElementById('review-name').required = true;
+
+  field.onchange = function () {
+    if (radio.value <= 3) {
+        document.getElementById('review-text').required = true;
+    }
+  };
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
