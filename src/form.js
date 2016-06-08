098a8cf020fc554b456f845610b5d74d57048922
loompa <anya_kasheeva@rambler.ru>
2016-06-08 17:07:22 +0300

    добавила появление-исчезовение "ссылок" на незаполненные-заполненные поля

--- src/form.js
+++ src/form.js
@@ -7,8 +7,30 @@
   var form = document.querySelector('.review-form');
   var field = form.querySelector('.review-form-group-mark');
   var radio = form.elements['review-mark'];
+  var name = form.elements['review-name'];
+  var text = form.elements['review-text'];
+  var textPrompt = document.querySelector('.review-fields-text');
+  var namePrompt = document.querySelector('.review-fields-name');
 
   document.getElementById('review-name').required = true;
+
+  form.oninput = function() {
+    changeVisible();
+  }
+
+  var changeVisible = function() {
+    if (name.value != '') {
+      namePrompt.classList.add('invisible');
+    } else {
+      namePrompt.classList.remove('invisible');
+    }
+
+    if (text.value != '') {
+      textPrompt.classList.add('invisible');
+    } else {
+      textPrompt.classList.remove('invisible');
+    }
+  }
 
   field.onchange = function() {
     if (radio.value <= 3) {
