f1acb1831462130d3230654d6435d81ea0ff01d7
loompa <anya_kasheeva@rambler.ru>
2016-06-14 21:45:57 +0300

    добавлена обработка загрузки изображений; исправлен класс review-rating на review-rating-one для последующего удобства

--- src/reviews.js
+++ src/reviews.js
@@ -1,23 +1,36 @@
 var reviewsContainer = document.querySelector('.reviews-list');
 var templateElement = document.querySelector('template');
+var reviewsFilter = document.querySelector('.reviews-filter');
+var IMAGE_WIDTH = 120;
+var IMAGE_HEIGHT = 120;
 
-//if ('content' in templateElement) {
-  elementToClone = templateElement.content.querySelector('.review');
-//} else {
-//  elementToClone = template.querySelector('.review');
-//}
+reviewsFilter.classList.add('invisible');
+
+var elementToClone = templateElement.content.querySelector('.review');
 
 var getReviewElement = function(data, container) {
 	var element = elementToClone.cloneNode(true);
 	element.querySelector('.review-text').textContent = data.description;
-	element.querySelector('.review-rating').textContent = data.rating;
-	element.querySelector('img').src = data.author.picture;
-	element.querySelector('img').alt = data.author.name;
+	element.querySelector('.review-rating-one').textContent = data.rating;
+	container.appendChild(element);
 
-    container.appendChild(element);
+	var reviewImage = new Image();
+
+	reviewImage.onload = function() {
+		element.querySelector('img').src = data.author.picture;
+		element.querySelector('img').width = IMAGE_WIDTH;
+		element.querySelector('img').height = IMAGE_HEIGHT;
+	};
+
+	reviewImage.onerror = function() {
+		element.classList.add('invisible');
+	};
+
+	reviewImage.src = data.author.picture;
+
     return element;
 };
 
-window.reviews.forEach(function(review, i, arr) {
+window.reviews.forEach(function(review) {
 	getReviewElement(review, reviewsContainer);
 });
