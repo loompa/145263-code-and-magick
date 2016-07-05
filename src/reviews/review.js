385465f529750306bb63f21b38d3632391fddea7
loompa <anya_kasheeva@rambler.ru>
2016-07-05 16:36:06 +0300

    методы объявляются в прототипе; убраны ссылки, сделанные через замыкание

--- src/reviews/review.js
+++ src/reviews/review.js
@@ -7,27 +7,31 @@ var Review = function(data, container) {
   this.data = data;
   this.element = reviewsMethods.getElement(this.data, container);
 
-  var quizAnswerNo = this.element.querySelector('.review-quiz-answer-no');
-  var quizAnswerYes = this.element.querySelector('.review-quiz-answer-yes');
+  this.quizAnswerNo = this.element.querySelector('.review-quiz-answer-no');
+  this.quizAnswerYes = this.element.querySelector('.review-quiz-answer-yes');
 
-  this.onYesClick = function() {
-    quizAnswerNo.classList.remove('review-quiz-answer-active');
-    quizAnswerYes.classList.add('review-quiz-answer-active');
-  };
+  this.onYesClick = this.onYesClick.bind(this);
+  this.onNoClick = this.onNoClick.bind(this);
+  this.remove = this.remove.bind(this);
 
-  this.onNoClick = function() {
-    quizAnswerYes.classList.remove('review-quiz-answer-active');
-    quizAnswerNo.classList.add('review-quiz-answer-active');
-  };
+  this.quizAnswerNo.addEventListener('click', this.onNoClick);
+  this.quizAnswerYes.addEventListener('click', this.onYesClick);
+};
 
-  this.remove = function() {
-    quizAnswerNo.removeEventListener('click', this.onYesClick);
-    quizAnswerYes.removeEventListener('click', this.onNoClick);
-    this.element.parentNode.removeChild(this.element);
-  };
+Review.prototype.onYesClick = function() {
+  this.quizAnswerNo.classList.remove('review-quiz-answer-active');
+  this.quizAnswerYes.classList.add('review-quiz-answer-active');
+};
 
-  quizAnswerNo.addEventListener('click', this.onNoClick);
-  quizAnswerYes.addEventListener('click', this.onYesClick);
+Review.prototype.onNoClick = function() {
+  this.quizAnswerYes.classList.remove('review-quiz-answer-active');
+  this.quizAnswerNo.classList.add('review-quiz-answer-active');
+};
+
+Review.prototype.remove = function() {
+  this.quizAnswerNo.removeEventListener('click', this.onYesClick);
+  this.quizAnswerYes.removeEventListener('click', this.onNoClick);
+  this.element.parentNode.removeChild(this.element);
 };
 
 module.exports = Review;
